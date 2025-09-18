4dc159208267c18e627047d837a55c4c106bf8f6
JuanMen23 <jm224304@fi365.ort.edu.uy>
2025-09-17 21:20:21 -0300

    now thread-safe

--- Server/Services/ClassService.cs
+++ Server/Services/ClassService.cs
@@ -1,23 +1,32 @@
 namespace Server.Services;
 
 using Server.ClassSession;
+using System.Collections.Concurrent;
+using System.Collections.Generic; 
+using System.Linq; 
 
 public class ClassService
 {
     private static readonly Lazy<ClassService> _instance = new(() => new ClassService());
     public static ClassService Instance => _instance.Value;
 
-    private readonly List<ClassSession> _classes = new();
+    private readonly ConcurrentBag<ClassSession> _classes = new();
     private int _nextId = 1;
+    private readonly object _lockNextId = new object();
 
     private ClassService() { }
 
     public ClassSession CreateClass(string name, string description, int maxSeats,
         DateTime startDateTime, int durationMinutes, string? imagePath)
     {
+        int id;
+        lock (_lockNextId)
+        {
+            id = _nextId++;
+        }
         var newClass = new ClassSession
         {
-            Id = _nextId++,
+            Id = id,
             Name = name,
             Description = description,
             MaxSeats = maxSeats,
@@ -31,5 +40,5 @@ public class ClassService
         return newClass;
     }
 
-    public IEnumerable<ClassSession> GetAllClasses() => _classes;
+    public IEnumerable<ClassSession> GetAllClasses() => _classes.ToList();
 }
